2e2fbd6eb9f8b0bf8ae82e3c88374c6030de6c17
Daniel Morais <danielmorais@outlook.com.br>
2018-07-02 22:17:56 -0300

    trab

---
+++ trabalho.py
@@ -0,0 +1,12 @@
+# hdfs dfs –put arquivo.txt          lancar arquivo para hdfs
+# spark-submit SimpleApp.py          submeter codigo ao hdfs
+
+"""SimpleApp.py"""
+
+from pyspark import SparkContext
+
+sc = SparkContext("local", "SimpleApp")
+logData = sc.textFile("hdfs://10.7.40.100:9000/user/engdados/input/WorldCups.csv")
+numAs = logData.filter(lambda s: 'c' in s).count()
+numBs = logData.filter(lambda s: 'd' in s).count()
+print "\n\nLines with c: % i, lines with d: % i\n" % (numAs, numBs)
