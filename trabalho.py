fdec7fb81bceb785fc52edbae6a920f3c1a5fce8
Daniel Morais <danielmorais@outlook.com.br>
2018-07-02 22:30:40 -0300

    trab

--- trabalho.py
+++ trabalho.py
@@ -6,7 +6,7 @@
 from pyspark import SparkContext
 
 sc = SparkContext("local", "SimpleApp")
-logData = sc.textFile("hdfs://10.7.40.100:9000/user/engdados/input/WorldCups.csv")
+logData = sc.textFile("hdfs://10.7.40.100:9000/user/engdados/input/Seattle_Traffic_Violations_-_2010.csv")
 numAs = logData.filter(lambda s: 'c' in s).count()
 numBs = logData.filter(lambda s: 'd' in s).count()
 print "\n\nLines with c: % i, lines with d: % i\n" % (numAs, numBs)
